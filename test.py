104831d6b50699ca494fd2f3fbbffe3ae96f8427
brianBoolean88 <brian.is.true.false@gmail.com>
2025-06-07 13:51:49 -0700

    Initialization Tests 🚀

---
+++ test.py
@@ -0,0 +1,17 @@
+bob=3
+
+if bob > 1:
+    print("lol!")
+
+
+for i in range(5):
+    print(i, end=" ")
+
+
+hi = [1,2,3,4,5]
+for i in hi:
+    print(i)
+
+str = "my name is george"
+g = str.split()
+print(g)
